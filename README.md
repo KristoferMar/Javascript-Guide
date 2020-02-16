d596de75d3b4a3c9982242cfba5c916e0f40c96a
KristoferMar <kristofer@brostu.dk>
2020-02-16 03:51:32 +0100

    created variables section

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Javascript knowledge central
+
+## Javascript fundamentals
+
+### Variables - var, let & const
+
+We should always go with let and cost as our variable choice. 
+
+- var has a more global scope which can introduce erros in many cases. Var's are changeable.
+- let has a more local scope which lets us control our variables better. It's also changeable.
+- const is a static constant which cannot be reassigned or changed once set. 
--- js_sandbox/variables.js
+++ js_sandbox/variables.js
@@ -1,1 +1,36 @@
 // var, let, const
+
+var name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+// Init a variable 
+var greeting; 
+greeting = 'Hello';
+
+// Variable rules
+// letters, numbers, _ , $
+// Cannot start with a number
+
+// Multi word vars
+var firstName = 'Jhon' // Camel case - sugusted
+var first_name = 'Sara' // Underscore
+var FirstName = 'Tome'; // Pascal case
+var firstname; // Lowercase 
+
+// LET
+let name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+// Const
+// Can not reassign
+// Have to assign a value, 
+const name2 = 'John Doe';
+console.log(name2);
+name2 = 'Steve Smith'; // Not possible
+console.log(name2);
+// We can change data inside an object but we can't reasign the objectsbundleRenderer.renderToStream
+// The same goes for arrays.
