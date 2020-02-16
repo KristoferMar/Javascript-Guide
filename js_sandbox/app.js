04321fba1c6cea5f82b76439bfd4f4d5991ff21b
KristoferMar <kristofer@brostu.dk>
2020-02-16 03:38:51 +0100

    initial commit

---
+++ js_sandbox/app.js
@@ -0,0 +1,18 @@
+//Log to console
+console.log('Hello world');
+console.log(123);
+console.log(true);
+var greetings = 'Hello';
+console.log(greetings);
+console.table({a:1, b:2})
+console.error('This is an error');
+// console.clear(); 
+console.warn('This is a warning')
+// Down below we log time between the two commands.
+console.time('Hello')
+console.timeEnd('Hello')
+
+/*
+ multi
+ line comments
+ */
