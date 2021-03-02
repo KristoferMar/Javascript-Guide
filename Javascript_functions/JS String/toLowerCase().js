78d16349edc0a2bca46f0ef73f86f13db3f10e0e
KristoferMar <kristofer@brostu.dk>
2021-03-02 13:50:32 +0100

    addition to the javascript section

---
+++ Javascript_functions/JS String/toLowerCase().js
@@ -0,0 +1,6 @@
+// This function converts a given string to lowercase letters.
+var str = "Hello World!";
+var res = str.toLowerCase();
+
+// Note: The toLowerCase() method does not change the original string.
+// Tip: Use the toUpperCase() method to convert a string to uppercase letters.
---
+++ Javascript_functions/JS String/toUpperCase().js
@@ -0,0 +1,6 @@
+// This function converts a given string to upper letters.
+var str = "Hello World!";
+var res = str.toUpperCase();
+
+// Note: The toUpperCase() method does not change the original string.
+// Tip: Use the toLowerCase() method to convert a string to lowercase letters.
