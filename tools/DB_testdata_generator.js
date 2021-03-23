2a90c9edf24c178015509231b1aca29b2a36645a
KristoferMar <kristofer@brostu.dk>
2021-03-23 15:01:10 +0100

    extended the tool to be able to output insert lines to a file

--- tools/DB_testdata_generator.js
+++ tools/DB_testdata_generator.js
@@ -1,4 +1,11 @@
-for (let value = 1; value < 100; value++) {
-    console.log(`INSERT  INTO dpreplica.dp_replica_002 (Modtager, Modtagertype, Indholdstype, Tilmeldt, SystemType)VALUES('SLETTET ${value}', 'P', 'S', 1, 'NEMSMS');`)
-}
+var fs = require("fs");
 
+// fs.truncate('DPtestdata.sql', 0, function(){console.log('truncated file')})
+
+console.log("WRITING TO FILE");
+for (let value = 1; value <= 10000; value++) {
+    // console.log(`INSERT  INTO dpreplica.dp_replica_002 (Modtager, Modtagertype, Indholdstype, Tilmeldt, SystemType)VALUES('SLETTET ${value}', 'P', 'S', 1, 'NEMSMS');`)
+    fs.appendFileSync('DPtestdata.sql', `INSERT  INTO dpreplica.dp_replica_002 (Modtager, Modtagertype, Indholdstype, Tilmeldt, SystemType)VALUES('SLETTET ${value}', 'P', 'S', 1, 'NEMSMS');`, function(err) {
+        if (err) return console.error(err);
+    });
+}
