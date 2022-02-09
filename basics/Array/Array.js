3cccdd4ecc63e05504efd908b1a5513344a23d39
KristoferMar <kristofer@brostu.dk>
2022-02-09 13:13:25 +0100

    exteded the way og adding to list

--- basics/Array/Array.js
+++ basics/Array/Array.js
@@ -1,5 +1,5 @@
 // Creation of different arrays
-const nummbers = [43, 56, 33, 23, 44, 36, 5];
+const numbers = [43, 56, 33, 23, 44, 36, 5];
 const numbers2 = new Array(22, 45, 33, 76, 54);
 const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
 const mixed = [22, 'Hello', true, undefined, null, {a:1, b:1}, new Date()];
@@ -42,7 +42,7 @@ val = fruit.sort();
 val = numbers.sort();
 
 // Use the "compare function"
-val = numbers.sort(function() {
+val = numbers.sort(function(x,y) {
     return x-y;
 })
 
@@ -59,4 +59,13 @@ function under50(num) {
 val = numbers.find(under50);
 
 // Check if array is empty
-if (arr.length === 0) { console.log("Array is empty!") }
+if (numbers.length === 0) { console.log("Array is empty!") }
+
+// Array using objects
+const emplyee = {name: 'anders', age: 23}
+const emplyee2 = {name: 'bende', age: 100}
+const emplyee3 = {name: 'bob', age: 53}
+
+let employeeArr = [emplyee, emplyee2, emplyee3];
+let employeeNewArr = [...employeeArr, {name: "anders2", age: 235}]
+console.log(employeeNewArr);
